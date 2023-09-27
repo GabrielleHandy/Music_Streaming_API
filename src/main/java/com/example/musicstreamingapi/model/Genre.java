27486d359a052e3d7548e34d2565725aee302dc3
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-27 15:45:50 -0500

    added getters and setters for Genre class

--- src/main/java/com/example/musicstreamingapi/model/Genre.java
+++ src/main/java/com/example/musicstreamingapi/model/Genre.java
@@ -11,14 +11,38 @@ public class Genre {
     @Column
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
-
+    @Column
     private String name;
-
+    @Column
     private String description;
-
+    @Column
     public Genre(Long id, String name, String description) {
         this.id = id;
         this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 }
