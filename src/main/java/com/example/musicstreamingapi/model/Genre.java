7e83626ee538ee3ed84c744d88aa9a6fc1d3fadf
GabrielleHandy <gehandy123@gmail.com>
2023-09-27 16:38:31 -0400

    Add Playist Model attributes and connections

--- src/main/java/com/example/musicstreamingapi/model/Genre.java
+++ src/main/java/com/example/musicstreamingapi/model/Genre.java
@@ -1,4 +1,4 @@
-package model;
+package com.example.musicstreamingapi.model;
 
 public class Genre {
 }
--- src/main/java/com/example/musicstreamingapi/model/Playlist.java
+++ src/main/java/com/example/musicstreamingapi/model/Playlist.java
@@ -1,4 +1,43 @@
 package com.example.musicstreamingapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "playlists")
 public class Playlist {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private LocalDate dateCreated = LocalDate.now();
+
+    @ManyToOne
+    @JoinColumn(name = "userProfile_id", nullable = false)
+    @JsonIgnore
+    private UserProfile userProfile;
+
+    //Many to Many Logic provided by Suresh Sigera and Bezdoker.com https://www.bezkoder.com/jpa-many-to-many/
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            })
+    @JoinTable(name = "playlist_songs",
+            joinColumns = { @JoinColumn(name = "playlist_id") },
+            inverseJoinColumns = { @JoinColumn(name = "song_id") })
+    private Set<Song> songs = new HashSet<>();
+
+
 }
