1ebdd79eeeec29c700c19e456d70cc4b2586575a
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-27 15:58:55 -0500

    added getters and setters to songs class

--- src/main/java/com/example/musicstreamingapi/model/Song.java
+++ src/main/java/com/example/musicstreamingapi/model/Song.java
@@ -20,5 +20,29 @@ public class Song {
         this.title = title;
         this.genre = genre;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
 
