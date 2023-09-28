6a466318dd464f039708fcfdaaeb3bd732d7cc72
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-28 09:22:24 -0700

    define private varianles and create logger inside JwtRequestFilter

--- src/main/java/com/example/musicstreamingapi/security/JwtRequestFilter.java
+++ src/main/java/com/example/musicstreamingapi/security/JwtRequestFilter.java
@@ -4,7 +4,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.persistence.Column;
+import java.util.logging.Logger;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
+
+    Logger logger = Logger.getLogger(JwtRequestFilter.class.getName());
+
+    private MyUserDetailsService myUserDetailsService;
+    private JWTUtils jwtUtils;
 }
