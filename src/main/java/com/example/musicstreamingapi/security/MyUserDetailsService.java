07fa16a5b26097cc37d045a45ebf18e80d719700
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-28 12:30:08 -0700

    create an instance of UserRepository inside UserService

--- src/main/java/com/example/musicstreamingapi/security/MyUserDetailsService.java
+++ src/main/java/com/example/musicstreamingapi/security/MyUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.example.musicstreamingapi.security;
 
+import com.example.musicstreamingapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
