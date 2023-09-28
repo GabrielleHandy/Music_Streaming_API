99b7077afccd1c0a3e4e2afffd284453d11069a5
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-28 09:24:37 -0700

    create method that checks for header Authorization and Bearer head token

--- src/main/java/com/example/musicstreamingapi/security/JwtRequestFilter.java
+++ src/main/java/com/example/musicstreamingapi/security/JwtRequestFilter.java
@@ -2,9 +2,11 @@ package com.example.musicstreamingapi.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.persistence.Column;
+import javax.servlet.http.HttpServletRequest;
 import java.util.logging.Logger;
 
 @Component
@@ -24,4 +26,14 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     public void setJwtUtils(JWTUtils jwtUtils) {
         this.jwtUtils = jwtUtils;
     }
+
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        // Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdXJlc2gyQGdhLmNvbSIsImlhdCI6MTY5NDgwMDAzNiwiZXhwIjoxNjk0ODg2NDM2fQ.z3smvkvDJqOYz7699UjvH5JQ51MuWL-KXffegc1UxWU
+        if (StringUtils.hasLength("headerAuth") && headerAuth.startsWith("Bearer")) {
+            return headerAuth.substring(7);
+        }
+        return null;
+    }
+
 }
