36f80113270234059452e1dd662f2610e6d3ec70
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-28 12:33:25 -0700

    annotate userservice with injectMock  so that the field automatically gets injected with mocks

--- src/test/java/com/example/musicstreamingapi/userTests/UserServiceTest.java
+++ src/test/java/com/example/musicstreamingapi/userTests/UserServiceTest.java
@@ -2,9 +2,11 @@ package com.example.musicstreamingapi.userTests;
 
 import com.example.musicstreamingapi.repository.UserRepository;
 import com.example.musicstreamingapi.service.UserService;
+import org.mockito.InjectMocks;
 import org.springframework.security.core.userdetails.User;
 
 public class UserServiceTest {
+    @InjectMocks
     private UserService userService;
 
     private UserRepository userRepository;
