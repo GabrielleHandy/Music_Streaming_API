0680d38e962288fd0dea42b9cfd04da52f3518da
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-27 20:10:52 -0700

    create params for User class

--- src/main/java/com/example/musicstreamingapi/model/User.java
+++ src/main/java/com/example/musicstreamingapi/model/User.java
@@ -4,30 +4,39 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
 
+/**
+ * Initliazes variables associated with the User class
+ * returns a one to one relationship with User Profile
+ * returns name, emailaddress, and password
+ */
 @Entity
-@Table(name = "user")
+@Table(name = "user")   // Define the "name" property and map it to a table column.
 public class User {
+
     @Column
     private String name;
 
+    // Define the "emailAddress" property, map it to a table column, and make it unique.
     @Column(unique = true)
     private String emailAddress;
+
+    // Define the "passWord" property, map it to a table column, and mark it for JSON serialization only.
     @Column
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String passWord;
 
+    // Define the "id" property and mark it as the primary key with automatic generation.
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // UserProfile
-   @OneToOne
-    @JoinColumn(name="profile_id",referencedColumnName = "id")
+    // Define a one-to-one relationship with UserProfile and specify the foreign key.
+    @OneToOne
+    @JoinColumn(name = "profile_id", referencedColumnName = "id")
     private UserProfile userProfile;
 
-    public User(){
-
+    public User() {
     }
 
     public User(String name, String emailAddress, String passWord) {
@@ -60,6 +69,7 @@ public class User {
         this.passWord = passWord;
     }
 
+    // toString() method
     @Override
     public String toString() {
         return "User{" +
@@ -72,8 +82,8 @@ public class User {
     public void setId(Long id) {
         this.id = id;
     }
-
     public Long getId() {
         return id;
     }
+
 }
