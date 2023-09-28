c162137e5fb4b1ff96571a61253c90013a557072
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-28 10:55:35 -0500

    Add PlaylistRepository interface

---
+++ src/main/java/com/example/musicstreamingapi/repository/PlaylistRepository.java
@@ -0,0 +1,9 @@
+package com.example.musicstreamingapi.repository;
+
+import com.example.musicstreamingapi.model.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaylistRepository extends JpaRepository <Playlist, Long> {
+}
