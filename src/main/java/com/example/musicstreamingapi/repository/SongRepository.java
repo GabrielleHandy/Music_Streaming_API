e579ed650f273ac7160237f6398dfc82ccd91823
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-28 10:49:42 -0500

    Add SongRepository interface

--- src/main/java/com/example/musicstreamingapi/repository/SongRepository.java
+++ src/main/java/com/example/musicstreamingapi/repository/SongRepository.java
@@ -1,10 +1,10 @@
 package com.example.musicstreamingapi.repository;
 
 import com.example.musicstreamingapi.model.Genre;
+import com.example.musicstreamingapi.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GenreRepository extends JpaRepository <Genre, Long> {
-
+public interface SongRepository extends JpaRepository<Song, Long> {
 }
