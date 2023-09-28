302f20f4e2523cd3147a9e3d34f3a81f390feb12
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-28 12:34:46 -0700

    annotate userrepository with mock so the field creates a mock interface

--- src/test/java/com/example/musicstreamingapi/userTests/UserServiceTest.java
+++ src/test/java/com/example/musicstreamingapi/userTests/UserServiceTest.java
@@ -3,12 +3,13 @@ package com.example.musicstreamingapi.userTests;
 import com.example.musicstreamingapi.repository.UserRepository;
 import com.example.musicstreamingapi.service.UserService;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.security.core.userdetails.User;
 
 public class UserServiceTest {
     @InjectMocks
     private UserService userService;
-
+    @Mock
     private UserRepository userRepository;
 
 
