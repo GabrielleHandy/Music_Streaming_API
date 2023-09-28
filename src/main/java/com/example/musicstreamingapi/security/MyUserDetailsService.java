a3f90ae1d0cf1a74b9c8779db1e79cc8aec4e668
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-28 08:55:39 -0700

    Instantiate userService and annotate to be authowired

--- src/main/java/com/example/musicstreamingapi/security/MyUserDetailsService.java
+++ src/main/java/com/example/musicstreamingapi/security/MyUserDetailsService.java
@@ -1,8 +1,14 @@
 package com.example.musicstreamingapi.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailsService {
     private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
