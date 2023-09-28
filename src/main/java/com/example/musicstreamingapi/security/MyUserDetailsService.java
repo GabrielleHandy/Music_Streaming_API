04bb69ecbfa0a899cab72da1139f77f538ca2fea
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-28 09:11:15 -0700

    create dependencies for User detials

--- src/main/java/com/example/musicstreamingapi/security/MyUserDetailsService.java
+++ src/main/java/com/example/musicstreamingapi/security/MyUserDetailsService.java
@@ -1,6 +1,7 @@
 package com.example.musicstreamingapi.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
