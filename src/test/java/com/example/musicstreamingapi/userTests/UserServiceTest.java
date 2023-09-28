25dcfdd5795c9d777834636f1c553ec768dffd75
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-28 12:32:18 -0700

    define variables of service and repository insde tests

--- src/test/java/com/example/musicstreamingapi/userTests/UserServiceTest.java
+++ src/test/java/com/example/musicstreamingapi/userTests/UserServiceTest.java
@@ -1,8 +1,13 @@
 package com.example.musicstreamingapi.userTests;
 
+import com.example.musicstreamingapi.repository.UserRepository;
+import com.example.musicstreamingapi.service.UserService;
 import org.springframework.security.core.userdetails.User;
 
 public class UserServiceTest {
+    private UserService userService;
+
+    private UserRepository userRepository;
 
 
 }
