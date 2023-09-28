085419cca833ebcde9b4dea5f05fc324218726ea
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-28 09:13:58 -0700

    create logger  to grab name inside JWT

--- src/main/java/com/example/musicstreamingapi/security/JWTUtils.java
+++ src/main/java/com/example/musicstreamingapi/security/JWTUtils.java
@@ -2,6 +2,9 @@ package com.example.musicstreamingapi.security;
 
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 public class JWTUtils {
+    Logger logger = Logger.getLogger(JWTUtils.class.getName());
 }
