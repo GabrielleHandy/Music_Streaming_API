47bdfbd3e13a4dcdead3811ea9b677ee471bda6c
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-28 13:00:18 -0700

    inside user service, create a method getUserById that finds the user by Id

--- src/main/java/com/example/musicstreamingapi/service/UserService.java
+++ src/main/java/com/example/musicstreamingapi/service/UserService.java
@@ -1,7 +1,10 @@
 package com.example.musicstreamingapi.service;
 
+import com.example.musicstreamingapi.model.User;
 import com.example.musicstreamingapi.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -9,5 +12,9 @@ public class UserService {
 
     public UserService(UserRepository userRepository){
         this.userRepository=userRepository;
+    }
+
+    public Optional<User> getUserById(Long userId) {
+        return userRepository.findById(userId);
     }
 }
