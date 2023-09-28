fc2dd0caf6aee84d14b9d60ebbb464827a36bca2
GabrielleHandy <gehandy123@gmail.com>
2023-09-28 15:30:21 -0400

    Autowire Playlist Repository in Playlist Service

--- src/main/java/com/example/musicstreamingapi/service/PlaylistService.java
+++ src/main/java/com/example/musicstreamingapi/service/PlaylistService.java
@@ -1,8 +1,19 @@
 package com.example.musicstreamingapi.service;
 
 import com.example.musicstreamingapi.model.Playlist;
+import com.example.musicstreamingapi.repository.PlaylistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PlaylistService {
+    private PlaylistRepository playlistRepository;
+
+    @Autowired
+    public void setPlaylistRepository(PlaylistRepository playlistRepository) {
+        this.playlistRepository = playlistRepository;
+    }
+
 }
