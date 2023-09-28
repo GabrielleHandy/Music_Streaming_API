6ccfe67611ff7c00a88ad3a53a06b51ab67c3bd3
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-28 12:11:49 -0500

    Add fields to SeedData class

--- src/main/java/com/example/musicstreamingapi/security/MyUserDetailsService.java
+++ src/main/java/com/example/musicstreamingapi/security/MyUserDetailsService.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MyUserDetailsService {
+public class MyUserDetailsService implements  {
     private UserService userService;
 
     @Autowired
--- src/main/java/com/example/musicstreamingapi/seed/SeedData.java
+++ src/main/java/com/example/musicstreamingapi/seed/SeedData.java
@@ -1,10 +1,22 @@
 package com.example.musicstreamingapi.seed;
 
+import com.example.musicstreamingapi.repository.GenreRepository;
+import com.example.musicstreamingapi.repository.PlaylistRepository;
+import com.example.musicstreamingapi.repository.SongRepository;
+import com.example.musicstreamingapi.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SeedData implements CommandLineRunner {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final GenreRepository genreRepository;
+    private final SongRepository genreRepository; // Updated repository name
+    private final PlaylistRepository bookRepository;
+
+
 
     @Override
     public void run(String... args) throws Exception {
