8d9b4cadcd0cbb5e9986a438b416d0111db0fd70
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-28 10:54:03 -0500

    updated SongRepository

--- src/main/java/com/example/musicstreamingapi/repository/SongRepository.java
+++ src/main/java/com/example/musicstreamingapi/repository/SongRepository.java
@@ -1,10 +1,9 @@
 package com.example.musicstreamingapi.repository;
 
-import com.example.musicstreamingapi.model.Genre;
 import com.example.musicstreamingapi.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SongRepository extends JpaRepository<Song, Long> {
+public interface SongRepository  extends JpaRepository <Song, Long> {
 }
