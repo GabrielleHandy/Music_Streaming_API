b27d3512ad2605fc469ea73becefe13d1d1ff0ba
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-28 09:22:50 -0500

    added documentation to Songs and Genre model

--- src/main/java/com/example/musicstreamingapi/model/Song.java
+++ src/main/java/com/example/musicstreamingapi/model/Song.java
@@ -1,6 +1,5 @@
 package com.example.musicstreamingapi.model;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
@@ -15,44 +14,84 @@ public class Song {
     @Column
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
+
     @Column
     private String title;
-
 
     @ManyToOne
     @JoinColumn(name = "genre_id")
     @JsonIgnore
     private Genre genre;
 
+    /**
+     * Default constructor for Song.
+     */
     public Song() {
-
     }
+
+    /**
+     * Constructor to initialize a Song with parameters.
+     *
+     * @param id    The unique identifier for the song.
+     * @param title The title of the song.
+     * @param genre The genre associated with the song.
+     */
     public Song(Long id, String title, Genre genre) {
         this.id = id;
         this.title = title;
         this.genre = genre;
     }
 
+    /**
+     * Get the unique identifier for the song.
+     *
+     * @return The song's unique identifier.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Set the unique identifier for the song.
+     *
+     * @param id The song's unique identifier.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Get the title of the song.
+     *
+     * @return The song's title.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Set the title of the song.
+     *
+     * @param title The song's title.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Get the genre associated with the song.
+     *
+     * @return The genre associated with the song.
+     */
     public Genre getGenre() {
         return genre;
     }
 
+    /**
+     * Set the genre associated with the song.
+     *
+     * @param genre The genre associated with the song.
+     */
     public void setGenre(Genre genre) {
         this.genre = genre;
     }
@@ -66,4 +105,3 @@ public class Song {
                 '}';
     }
 }
-
