7a80af567f9200d1278a5cefe49fd0f93c0a1dea
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-29 10:18:10 -0500

    Add a passing test for get all songs by GenreId

--- src/main/java/com/example/musicstreamingapi/service/SongService.java
+++ src/main/java/com/example/musicstreamingapi/service/SongService.java
@@ -5,7 +5,7 @@ import com.example.musicstreamingapi.repository.GenreRepository;
 import com.example.musicstreamingapi.repository.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.example.musicstreamingapi.model.Genre;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,41 +20,26 @@ public class SongService {
     public void setSongRepository(SongRepository songRepository) {
         this.songRepository = songRepository;
     }
+
     @Autowired
     public void setGenreRepository(GenreRepository genreRepository) {
         this.genreRepository = genreRepository;
     }
 
-
+    public List<Genre> getGenres() {
+        return genreRepository.findAll();
+    }
 
     public SongService(List<Song> songsList) {
         this.songsList = songsList; // Initialize the list of songs in the constructor
     }
 
-    public List<Song>  getAllSongs() {
+    public List<Song> getAllSongs() {
         return songRepository.findAll(); // Return the list of songs
     }
 
-    public Song createSong(Song song) { //creates a new Song object, and saves it to the database
-        return songRepository.save(song);
-    }
 
-    public Song editSong(long songId, String editedSongName) {
-        Optional<Song> optionalSong = songRepository.findById(songId);
-        if (optionalSong.isPresent()) {
-            Song existingSong = optionalSong.get();
-            existingSong.setTitle(editedSongName);
-            return songRepository.save(existingSong);
-        } else {
-            throw new IllegalArgumentException("Song not found with ID: " + songId + "not found");
-        }
-    }
-
-    public Song deleteSong(long songId) {
-        Song deletedSong = songRepository.findById(songId)
-                .orElseThrow(() -> new IllegalArgumentException("Song not found with ID: " + songId));
-
-        songRepository.delete(deletedSong);
-        return deletedSong;
+    public List<Song> getAllSongsByGenreId(Long genreId) {
+        return songRepository.findByGenreId(genreId);
     }
 }
