e17e1d3e84b1c2708f0ea0c21789745b74be766d
GabrielleHandy <gehandy123@gmail.com>
2023-09-29 14:43:53 -0400

    Autowiring and adding MockBean to PlaylistControllerTest

--- src/test/java/com/example/musicstreamingapi/controllerTests/PlaylistControllerTests.java
+++ src/test/java/com/example/musicstreamingapi/controllerTests/PlaylistControllerTests.java
@@ -1,7 +1,23 @@
 package com.example.musicstreamingapi.controllerTests;
 
+import com.example.musicstreamingapi.controller.PlaylistController;
+import com.example.musicstreamingapi.model.Playlist;
+import com.example.musicstreamingapi.service.PlaylistService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(PlaylistController.class)
 public class PlaylistControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    private PlaylistService playlistService;
+
 }
