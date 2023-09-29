8d6f769223dee454825a152e4b7c898d456092fb
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-29 12:22:15 -0700

    define setup to be annotated with @BeforeEach so that UserController builds

--- src/test/java/com/example/musicstreamingapi/controllerTests/UserControllerTests.java
+++ src/test/java/com/example/musicstreamingapi/controllerTests/UserControllerTests.java
@@ -2,9 +2,12 @@ package com.example.musicstreamingapi.controllerTests;
 
 import com.example.musicstreamingapi.controller.UserController;
 import com.example.musicstreamingapi.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 public class UserControllerTests {
     @InjectMocks
@@ -12,6 +15,13 @@ public class UserControllerTests {
     @Mock
     private UserService userService;
     private MockMvc mockMvc;
+
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+    }
 
 
 
