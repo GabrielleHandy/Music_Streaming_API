e18aa5dc36e94aef18715e83219ab18373b92e18
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-29 12:19:33 -0700

    create mocks inside controller tests

--- src/test/java/com/example/musicstreamingapi/controllerTests/UserControllerTests.java
+++ src/test/java/com/example/musicstreamingapi/controllerTests/UserControllerTests.java
@@ -1,5 +1,18 @@
 package com.example.musicstreamingapi.controllerTests;
 
+import com.example.musicstreamingapi.controller.UserController;
+import com.example.musicstreamingapi.service.UserService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.web.servlet.MockMvc;
+
 public class UserControllerTests {
-    private
+    @InjectMocks
+    private UserController userController;
+    @Mock
+    private UserService userService;
+    private MockMvc mockMvc;
+
+
+
 }
