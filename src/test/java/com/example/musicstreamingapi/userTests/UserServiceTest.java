c6628fac8c13aa95aa026893c7db6e8a71c91ec4
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-29 08:18:21 -0700

    create doc strings for getUserbyId, userbyIdNotFound, anc createUserTEST

--- src/test/java/com/example/musicstreamingapi/userTests/UserServiceTest.java
+++ src/test/java/com/example/musicstreamingapi/userTests/UserServiceTest.java
@@ -26,22 +26,29 @@ public class UserServiceTest {
     public void setUp(){
         MockitoAnnotations.initMocks(this);
     }
+    /**
+     * This test method verifies the functionality of the code getUserById method
+     * in the UserService class. It checks if the method correctly retrieves a user
+     * by their ID from the repository and returns the expected user details.
+     */
     @Test
     public void testGetUserById(){
-
         User user = new User(1L, "Marco", "marco@example.com", "password123");
-
+        // Call the UserRepository to return the user when findById is called
         when(userRepository.findById(userId)).thenReturn(Optional.of(user));
-
+        // Call the getUserById method to retrieve a user by their ID
         Optional<User> result = userService.getUserById(userId);
-
         verify(userRepository, times(1)).findById(userId);
         assertTrue(result.isPresent());
-
         assertEquals("Marco", result.get().getName());
         assertEquals("marco@example.com",result.get().getEmailAddress());
-
     }
+    /**
+     * This test method verifies the behavior of getUserById method.
+     * UserService class attempts to retrieve a user by an
+     * ID that does not exist in the repository. It checks if the method correctly
+     * handles the case where the user is not found and returns an optional.
+     */
     @Test
     public void testGetUserByIdNotFound() {
         Long userId = 77L;
@@ -53,17 +60,18 @@ public class UserServiceTest {
         assertFalse(result.isPresent());
     }
 
-    // GETALLUSER PROFILEs
-
-
-    //CreateUser
-@Test
+    /**
+     * This test method verifies the functionality of the createUser method
+     * in the UserService class. It checks if the method correctly creates
+     * a new user by saving the provided user data and returns the new created user
+     */
+    @Test
     public void testCreateUser(){
         User userToCreate = new User(userId,"Betselot","Bets@gmail.com","newpassword456");
+        // Configure the UserRepository to return the same user when save is called
         when(userRepository.save(userToCreate)).thenReturn(userToCreate);
-
+        // Call the createUser method to create a new user
         User createdUser = userService.createUser(userToCreate);
-
         verify(userRepository,times(1)).save(userToCreate);
         assertNotNull(createdUser.getId());
         assertEquals("Betselot",createdUser.getName());
@@ -98,8 +106,6 @@ public class UserServiceTest {
             Long userIdToDelete = userId;
             userService.deleteUser(userIdToDelete);
             verify(userRepository, times(1)).deleteById(userIdToDelete);
-
-
         }
 
 
