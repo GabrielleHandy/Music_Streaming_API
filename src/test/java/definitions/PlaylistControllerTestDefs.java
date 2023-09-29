514a0fed85482ffaab33f195e94aee9f6c3088cf
GabrielleHandy <gehandy123@gmail.com>
2023-09-29 19:05:04 -0400

    Cucumber layout fixed

--- src/test/java/definitions/PlaylistControllerTestDefs.java
+++ src/test/java/definitions/PlaylistControllerTestDefs.java
@@ -9,53 +9,54 @@ public class PlaylistControllerTestDefs {
 
     @Given("A list of playlists are available")
     public void aListOfPlaylistsAreAvailable() {
+
     }
 
-    @When("I create a playlist")
-    public void iCreateAPlaylist() {
-    }
-
-    @Then("The playlist is created")
-    public void thePlaylistIsCreated() {
-    }
-
-    @When("I remove playlist from my list of playlists")
-    public void iRemovePlaylistFromMyListOfPlaylists() {
-    }
-
-    @When("I update a playlist")
-    public void iUpdateAPlaylist() {
-    }
-
-    @Then("The playlist is updated")
-    public void thePlaylistIsUpdated() {
-    }
-
-    @Given("A list of songs are available in a playlist")
-    public void aListOfSongsAreAvailableInAPlaylist() {
-    }
-
-    @When("I add a song to the playlist")
-    public void iAddASongToThePlaylist() {
-    }
-
-    @Then("The song is added to playlist")
-    public void theSongIsAddedToPlaylist() {
-    }
-
-    @When("I remove a song from playlist")
-    public void iRemoveASongFromPlaylist() {
-    }
-
-    @Then("The song is removed")
-    public void theSongIsRemoved() {
-    }
-
-    @When("I update a playlist name")
-    public void iUpdateAPlaylistName() {
-    }
-
-    @Then("The playlist name is updated")
-    public void thePlaylistNameIsUpdated() {
-    }
+//    @When("I create a playlist")
+//    public void iCreateAPlaylist() {
+//    }
+//
+//    @Then("The playlist is created")
+//    public void thePlaylistIsCreated() {
+//    }
+//
+//    @When("I remove playlist from my list of playlists")
+//    public void iRemovePlaylistFromMyListOfPlaylists() {
+//    }
+//
+//    @When("I update a playlist")
+//    public void iUpdateAPlaylist() {
+//    }
+//
+//    @Then("The playlist is updated")
+//    public void thePlaylistIsUpdated() {
+//    }
+//
+//    @Given("A list of songs are available in a playlist")
+//    public void aListOfSongsAreAvailableInAPlaylist() {
+//    }
+//
+//    @When("I add a song to the playlist")
+//    public void iAddASongToThePlaylist() {
+//    }
+//
+//    @Then("The song is added to playlist")
+//    public void theSongIsAddedToPlaylist() {
+//    }
+//
+//    @When("I remove a song from playlist")
+//    public void iRemoveASongFromPlaylist() {
+//    }
+//
+//    @Then("The song is removed")
+//    public void theSongIsRemoved() {
+//    }
+//
+//    @When("I update a playlist name")
+//    public void iUpdateAPlaylistName() {
+//    }
+//
+//    @Then("The playlist name is updated")
+//    public void thePlaylistNameIsUpdated() {
+//    }
 }
