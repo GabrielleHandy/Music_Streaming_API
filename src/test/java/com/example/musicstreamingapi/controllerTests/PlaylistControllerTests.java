62e53f0b56def839d9af996ac8a80656a8e6d3db
GabrielleHandy <gehandy123@gmail.com>
2023-09-29 14:45:37 -0400

    add test Data to PlaylistControllerTest

--- src/test/java/com/example/musicstreamingapi/controllerTests/PlaylistControllerTests.java
+++ src/test/java/com/example/musicstreamingapi/controllerTests/PlaylistControllerTests.java
@@ -2,12 +2,19 @@ package com.example.musicstreamingapi.controllerTests;
 
 import com.example.musicstreamingapi.controller.PlaylistController;
 import com.example.musicstreamingapi.model.Playlist;
+import com.example.musicstreamingapi.model.UserProfile;
 import com.example.musicstreamingapi.service.PlaylistService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 
 @WebMvcTest(PlaylistController.class)
 public class PlaylistControllerTests {
@@ -19,5 +26,9 @@ public class PlaylistControllerTests {
 
     @MockBean
     private PlaylistService playlistService;
-
+    private final UserProfile testUserProfile = new UserProfile(1L, "TestUser", "Test", "TestBio", null);
+    private final Playlist testPlaylist1 = new Playlist(1L, "Test Playlist", LocalDate.now(), testUserProfile, new HashSet<>());
+    private final Playlist testPlaylist2 = new Playlist(2L, "Test Playlist", LocalDate.now(), testUserProfile, new HashSet<>());
+    private final Playlist testPlaylist3 = new Playlist(3L, "Test Playlist", LocalDate.now(), testUserProfile, new HashSet<>());
+    private final List<Playlist> testListPlaylist = new ArrayList<>(Arrays.asList(testPlaylist1, testPlaylist2, testPlaylist3));
 }
