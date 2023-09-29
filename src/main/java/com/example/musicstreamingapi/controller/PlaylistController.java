9ffb4d4a6c8d01a7fa5a0f9600d08a998cb14426
GabrielleHandy <gehandy123@gmail.com>
2023-09-29 14:41:55 -0400

    Create playlist controller

---
+++ src/main/java/com/example/musicstreamingapi/controller/PlaylistController.java
@@ -0,0 +1,9 @@
+package com.example.musicstreamingapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/playlists")
+public class PlaylistController {
+}
--- src/test/java/com/example/musicstreamingapi/controllerTests/PlaylistControllerTests.java
+++ src/test/java/com/example/musicstreamingapi/controllerTests/PlaylistControllerTests.java
@@ -1,4 +1,7 @@
 package com.example.musicstreamingapi.controllerTests;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(PlaylistController.class)
 public class PlaylistControllerTests {
 }
