471d2d1f33aff1c6b556d273f2c0cc290a6da743
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-29 12:30:42 -0700

    implement a when we grab the userID inside service, return the user

--- src/test/java/com/example/musicstreamingapi/controllerTests/UserControllerTests.java
+++ src/test/java/com/example/musicstreamingapi/controllerTests/UserControllerTests.java
@@ -1,13 +1,19 @@
 package com.example.musicstreamingapi.controllerTests;
 
 import com.example.musicstreamingapi.controller.UserController;
+import com.example.musicstreamingapi.model.User;
 import com.example.musicstreamingapi.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 public class UserControllerTests {
     @InjectMocks
@@ -23,6 +29,22 @@ public class UserControllerTests {
         mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
     }
 
+    //TestGetUserID
+
+    @Test
+    public void testGetUserByTheirId() throws Exception{
+        Long userId = 1L;
+        User user = new User(userId,"Marco","marco@gmail.com","password123",null);
+
+        when(userService.getUserById(userId)).thenReturn(Optional.of(user));
+
+
+
+    }
+
+
+
+    //TestGetUserIdNotFound
 
 
 }
