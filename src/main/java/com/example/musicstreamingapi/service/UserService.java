5ad0d80bc2d3eb9c0c79ea542566daa50ddf4f9d
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-09-29 08:59:14 -0700

    create delete doc strings

--- src/main/java/com/example/musicstreamingapi/service/UserService.java
+++ src/main/java/com/example/musicstreamingapi/service/UserService.java
@@ -1,12 +1,9 @@
 package com.example.musicstreamingapi.service;
-
 import com.example.musicstreamingapi.model.User;
 import com.example.musicstreamingapi.repository.UserRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
-
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -37,17 +34,25 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    /**
+     * Updates an existing user's information with the provided data and saves the changes to the UserRepository.
+     * This method checks if a user with the given ID exists, and if so, it updates the specified fields (name, email address, and password)
+     * with the values from the provided updatedUser object. The updated user is then saved to the repository.
+     * If no user with the given ID is found, an IllegalArgumentException is thrown.
+     * @param userId The unique identifier (ID) of the user to update.
+     * @param updatedUser The user object containing the updated information.
+     * @return The updated user object with the changes saved.
+     * @throws IllegalArgumentException if no user with the given ID is found.
+     */
     public User updateUser(Long userId, User updatedUser) {
         // Check if the user with the given ID exists
         Optional<User> existingUserOptional = userRepository.findById(userId);
         if (existingUserOptional.isPresent()) {
             User existingUser = existingUserOptional.get();
-
             // Update the fields you want to change
             existingUser.setName(updatedUser.getName());
             existingUser.setEmailAddress(updatedUser.getEmailAddress());
             existingUser.setPassWord(updatedUser.getPassWord());
-
             // Save the updated user
             return userRepository.save(existingUser);
         } else {
@@ -55,8 +60,11 @@ public class UserService {
             throw new IllegalArgumentException();
         }
     }
-
-
+    /**
+     * Deletes a user with the specified unique identifier (ID) from the UserRepository.
+     * This method deletes the user with the given ID from the repository using the deleteById method.
+     * @param userId The unique identifier (ID) of the user to delete.
+     */
     public  void deleteUser(Long userId){
         userRepository.deleteById(userId);
     }
