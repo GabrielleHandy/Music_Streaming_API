b0272b00be99fd64643b253d91c39624cb42b5e1
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-29 18:09:36 -0500

    added a mock mcv for song controller test

--- src/test/java/com/example/musicstreamingapi/controllerTests/SGControllerTest.java
+++ src/test/java/com/example/musicstreamingapi/controllerTests/SGControllerTest.java
@@ -8,40 +8,55 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+@WebMvcTest(SongGenreController.class)
 public class SGControllerTest {
+
+    @MockBean
+    private SongService songService;
+
     @InjectMocks
     private SongGenreController songGenreController;
 
-    @Mock
-    private SongService songServiceMock;
+    private MockMvc mockMvc;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(songGenreController).build();
     }
 
-    @Test
-    public void testGetAllSongsNotEmpty() {
-        List<Song> songsList = Collections.singletonList(new Song());
-        when(songServiceMock.getAllSongs()).thenReturn(songsList);
-        ResponseEntity<?> responseEntity = songGenreController.getAllSongs();
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-    }
+    Song testSong1 = new Song(1L, "Test Song 1", null);
+    Song testSong2 = new Song(2L, "Test Song 2", null);
+    Song testSong3 = new Song(3L, "Test Song 3", null);
 
     @Test
-    public void testGetAllSongsEmpty() {
-        List<Song> emptySongsList = Collections.emptyList();
-        when(songServiceMock.getAllSongs()).thenReturn(emptySongsList);
-        ResponseEntity<?> responseEntity = songGenreController.getAllSongs();
-        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+    public void getAllSongs() throws Exception {
+        List<Song> songList = new ArrayList<>(Arrays.asList(testSong1, testSong2, testSong3));
+        when(songService.getAllSongs()).thenReturn(songList);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/songs/")
+                        .with(jwt().jwt(builder -> builder.tokenValue("value").claim("data", "data1").header("Authorization", "Bearer value")))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data", hasSize(3)))
+                .andExpect(jsonPath("$.message").value("Success"))
+                .andDo(print());
     }
 }
