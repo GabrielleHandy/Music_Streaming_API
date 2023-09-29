64954c823f426c5a2d775c73bc1c37349ff07e0f
GabrielleHandy <gehandy123@gmail.com>
2023-09-29 17:51:30 -0400

    Playlist controller

--- src/test/java/com/example/musicstreamingapi/controllerTests/PlaylistControllerTests.java
+++ src/test/java/com/example/musicstreamingapi/controllerTests/PlaylistControllerTests.java
@@ -2,19 +2,42 @@ package com.example.musicstreamingapi.controllerTests;
 
 import com.example.musicstreamingapi.controller.PlaylistController;
 import com.example.musicstreamingapi.model.Playlist;
+import com.example.musicstreamingapi.model.User;
 import com.example.musicstreamingapi.model.UserProfile;
+import com.example.musicstreamingapi.security.*;
 import com.example.musicstreamingapi.service.PlaylistService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.oauth2.jwt.Jwt.Builder;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import javax.servlet.FilterChain;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.get;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PlaylistController.class)
 public class PlaylistControllerTests {
@@ -24,11 +47,39 @@ public class PlaylistControllerTests {
     @Autowired
     ObjectMapper objectMapper;
 
+
+    @MockBean
+    JwtRequestFilter jwtRequestFilter;
     @MockBean
     private PlaylistService playlistService;
+    @MockBean
+    private JWTUtils jwtUtils;
+    @MockBean
+    private SecurityConfiguration securityConfiguration;
+    @MockBean
+    private MyUserDetailsService userDetailsService;
+
+
     private final UserProfile testUserProfile = new UserProfile(1L, "TestUser", "Test", "TestBio", null);
+    private final User testUser = new User(1L, "TestUser", "test@test.com", "1234", testUserProfile);
     private final Playlist testPlaylist1 = new Playlist(1L, "Test Playlist", LocalDate.now(), testUserProfile, new HashSet<>());
     private final Playlist testPlaylist2 = new Playlist(2L, "Test Playlist", LocalDate.now(), testUserProfile, new HashSet<>());
     private final Playlist testPlaylist3 = new Playlist(3L, "Test Playlist", LocalDate.now(), testUserProfile, new HashSet<>());
     private final List<Playlist> testListPlaylist = new ArrayList<>(Arrays.asList(testPlaylist1, testPlaylist2, testPlaylist3));
+
+
+
+    @Test
+    void shouldReturnListPlaylist() throws Exception {
+        when(playlistService.getAllPlaylists()).thenReturn(testListPlaylist);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/playlists/")
+                //Help from stackoverflow:
+                .with(jwt().jwt(builder -> builder.tokenValue("value").claim("data", "data1").header("Authorization", "Bearer value")))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data", hasSize(3)))
+                .andDo(print());
+
+    }
 }
