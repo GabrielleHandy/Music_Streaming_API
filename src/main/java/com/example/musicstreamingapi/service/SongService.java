870869caadeb667c8009b441813b1090d0d834a3
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-29 01:50:18 -0500

    Introduce GenreRepository in SongService

--- src/main/java/com/example/musicstreamingapi/service/SongService.java
+++ src/main/java/com/example/musicstreamingapi/service/SongService.java
@@ -1,6 +1,7 @@
 package com.example.musicstreamingapi.service;
 
 import com.example.musicstreamingapi.model.Song;
+import com.example.musicstreamingapi.repository.GenreRepository;
 import com.example.musicstreamingapi.repository.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,19 @@ import java.util.Optional;
 @Service
 public class SongService {
     private SongRepository songRepository;
+    private GenreRepository genreRepository; //ADD
     private final List<Song> songsList; // Define the list of songs
+
     @Autowired
     public void setSongRepository(SongRepository songRepository) {
         this.songRepository = songRepository;
     }
+    @Autowired
+    public void setGenreRepository(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
+
+
 
     public SongService(List<Song> songsList) {
         this.songsList = songsList; // Initialize the list of songs in the constructor
