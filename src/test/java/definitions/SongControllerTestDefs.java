fe4f5b17351d2fe3323fd958329c622462e1a003
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-30 16:42:53 -0500

    Configure Cucumber context and set up Spring Boot test environment for SongController

--- src/test/java/definitions/SongControllerTestDefs.java
+++ src/test/java/definitions/SongControllerTestDefs.java
@@ -1,23 +1,34 @@
 package definitions;
 
+import com.example.musicstreamingapi.MusicStreamingApiApplication;
+import io.cucumber.java.en.Given;
+import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.Assert;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
+import static java.util.logging.Logger.getLogger;
+
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = MusicStreamingApiApplication.class)
 public class SongControllerTestDefs {
 
-    private static final String Base_URL = "http://localhost:";
+    private static final String BASE_URL = "http://localhost:";
 
-    //Logger initialization
-    private static final Logger log = Logger.getLogger(SongControllerTestDefs.class.getName());
+    // Logger initialization
+    private static final Logger log = getLogger(SongControllerTestDefs.class.getName());
 
-    @LocalServerPort
-    String port;
 
-    private static Response response;
-    public SongControllerTestDefs() {
-        Given("^A list of songs are available in genre"), () -> {
-        });
-    }
+
 }
