68317a35874b0274c81ce547efd545acbb447299
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-30 22:09:10 -0500

    Add endpoint in Song Controller to retrieve songs by genre ID

--- src/main/java/com/example/musicstreamingapi/controller/SongGenreController.java
+++ src/main/java/com/example/musicstreamingapi/controller/SongGenreController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,13 +25,29 @@ public class SongGenreController {
         this.songService = songService;
     }
 
-    @GetMapping
+    // Get all songs
+    @GetMapping//"/api/songs"
     public ResponseEntity<?> getAllSongs() {
         List<Song> songsList = songService.getAllSongs();
         HashMap<String, Object> message = new HashMap<>();
 
         if (songsList.isEmpty()) {
             message.put("message", "Cannot find any songs.");
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        } else {
+            message.put("message", "Success");
+            message.put("data", songsList);
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
+    }
+
+    // Get songs by genre ID
+    @GetMapping("/Genre/{genreId}")
+    public ResponseEntity<?> getSongsByGenre(@PathVariable Long genreId) {
+        List<Song> songsList = songService.getAllSongsByGenreId(genreId);
+        HashMap<String, Object> message = new HashMap<>();
+        if (songsList.isEmpty()) {
+            message.put("message", "No songs found for the given genre ID.");
             return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
         } else {
             message.put("message", "Success");
