5bfd10bbf16cac93fccf041381ef3c4e22b4bb6a
GabrielleHandy <gehandy123@gmail.com>
2023-09-30 22:34:34 -0400

    "Add OpenApi Documentation to PlaylistController"

--- src/main/java/com/example/musicstreamingapi/controller/PlaylistController.java
+++ src/main/java/com/example/musicstreamingapi/controller/PlaylistController.java
@@ -7,6 +7,9 @@ import com.example.musicstreamingapi.security.MyUserDetailsService;
 import com.example.musicstreamingapi.security.SecurityConfiguration;
 import com.example.musicstreamingapi.service.PlaylistService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,7 +31,7 @@ public class PlaylistController {
     }
 
     private HashMap<String, Object> response = new HashMap<>();
-
+    @Operation(summary = "Get all playlists", description = "Retrieve a list of all playlists.")
     @GetMapping("/")
     public ResponseEntity<?> getAllPlaylists(){
         List<Playlist> playListList = playlistService.getAllPlaylists();
@@ -37,17 +40,22 @@ public class PlaylistController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @Operation(summary = "Create a playlist", description = "Create a new playlist.")
+    @ApiResponse(responseCode = "201", description = "Successfully created playlist")
     @PostMapping("/")
     public ResponseEntity<?> createPlaylist(@RequestBody Playlist playlist){
+
         response.clear();
         Playlist createdPlayList = playlistService.createPlaylist(playlist);
         response.put("data", createdPlayList);
         response.put("message", "Successfully created playlist named " + createdPlayList.getName());
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
-
+    @Operation(summary = "Update a playlist by ID", description = "Update an existing playlist by ID.")
     @PutMapping("/{playlistId}/")
-    public ResponseEntity<?> updatePlaylist(@PathVariable(name = "playlistId") Long playlistId, @RequestBody Playlist playlist){
+    public ResponseEntity<?> updatePlaylist(
+            @Parameter(description = "ID of the playlist to be updated.", required = true)
+            @PathVariable(name = "playlistId") Long playlistId, @RequestBody Playlist playlist){
         response.clear();
         Playlist updatedPlayList = playlistService.updatePlaylist(playlistId, playlist);
 
@@ -56,8 +64,12 @@ public class PlaylistController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete a playlist by ID", description = "Delete an existing playlist by ID.")
+    @ApiResponse(responseCode = "200", description = "Successfully deleted playlist")
     @DeleteMapping("/{playlistId}/")
-    public ResponseEntity<?> deletePlaylist(@PathVariable(name = "playlistId") Long playlistId){
+    public ResponseEntity<?> deletePlaylist(
+            @Parameter(description = "ID of the playlist to be deleted.", required = true)
+            @PathVariable(name = "playlistId") Long playlistId){
         response.clear();
         Playlist deletedPlayList = playlistService.deletePlaylist(playlistId);
 
@@ -66,24 +78,41 @@ public class PlaylistController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get all songs in a playlist by ID", description = "Retrieve all songs in a playlist by ID.")
     @GetMapping("/{playlistId}/songs/")
-    public ResponseEntity<?> getPlaylistSongs(@PathVariable(name = "playlistId") Long playlistId){
+    public ResponseEntity<?> getPlaylistSongs(
+            @Parameter(description = "ID of the playlist to retrieve songs from.", required = true)
+            @PathVariable(name = "playlistId") Long playlistId){
         response.clear();
 
         response.put("data", playlistService.getAllSongsInPlaylist(playlistId));
         response.put("message", "Success");
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
+    @Operation(summary = "Add a song to a playlist by ID", description = "Add a song to a playlist by specifying both playlist and song IDs.")
+    @ApiResponse(responseCode = "200", description = "Song successfully added")
     @PostMapping("/{playlistId}/songs/{songId}/")
-    public ResponseEntity<?> addSongToPlaylist(@PathVariable(name = "playlistId") Long playlistId, @PathVariable(name = "songId") Long songId){
+    public ResponseEntity<?> addSongToPlaylist(
+            @Parameter(description = "ID of the playlist to add the song to.", required = true)
+            @PathVariable(name = "playlistId") Long playlistId,
+            @Parameter(description = "ID of the song to be added to the playlist.", required = true)
+            @PathVariable(name = "songId") Long songId){
         response.clear();
         Playlist updatedPlaylist = playlistService.addSongToPlaylist(playlistId,songId);
         response.put("data", updatedPlaylist);
         response.put("message", "Song successfully added");
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
+    @Operation(summary = "Remove a song from a playlist by ID", description = "Remove a song from a playlist by specifying both playlist and song IDs.")
+    @ApiResponse(responseCode = "200", description = "Song removed from the playlist")
     @DeleteMapping("/{playlistId}/songs/{songId}/")
-    public ResponseEntity<?> removeSongFromPlaylist(@PathVariable(name = "playlistId") Long playlistId, @PathVariable(name = "songId") Long songId){
+    public ResponseEntity<?> removeSongFromPlaylist(
+            @Parameter(description = "ID of the playlist to add the song to.", required = true)
+            @PathVariable(name = "playlistId") Long playlistId,
+            @Parameter(description = "ID of the song to be added to the playlist.", required = true)
+            @PathVariable(name = "songId") Long songId){
         response.clear();
         Playlist updatedPlaylist = playlistService.removeSongFromPlaylist(playlistId,songId);
         response.put("data", updatedPlaylist);
