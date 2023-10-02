789ae9790bd601d4c98e76ec29569c67a117c8dc
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-01 20:56:02 -0700

    iCreateAnAccount test passes

--- src/test/java/definitions/UserControllerTestDefs.java
+++ src/test/java/definitions/UserControllerTestDefs.java
@@ -4,6 +4,8 @@ import com.example.musicstreamingapi.MusicStreamingApiApplication;
 import com.example.musicstreamingapi.model.User;
 import com.example.musicstreamingapi.service.UserService;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -13,7 +15,9 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 
 import java.util.logging.Logger;
 
-@CucumberContextConfiguration
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = MusicStreamingApiApplication.class)
 public class UserControllerTestDefs {
     private static final Logger logger = Logger.getLogger(PlaylistControllerTestDefs.class.getName());
@@ -31,7 +35,23 @@ public class UserControllerTestDefs {
     private Long userId;
     private String loginEmailAddress;
     private String loginPassword;
+    @Given("A list of Users")
+    public void aListOfUsers() {
 
+    }
 
+    @When("I create an account.")
+    public void iCreateAnAccount() {
+        User newUser = new User();
+        newUser.setName("John Doe");
+        newUser.setEmailAddress("johndoe@example.com");
+        newUser.setPassword("password123");
 
+        createdUser = userService.createUser(newUser);
+        assertNotNull(createdUser);
+    }
+
+    @Then("I get an account and user profile")
+    public void iGetAnAccountAndUserProfile() {
+    }
 }
