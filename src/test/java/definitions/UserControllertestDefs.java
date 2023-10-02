90f2cae71a6c5a3a4ba155ad0cf098537253aeed
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-10-01 20:38:32 -0700

    create variables inside of my usertestdefs class

--- src/test/java/definitions/UserControllertestDefs.java
+++ src/test/java/definitions/UserControllertestDefs.java
@@ -1,7 +1,11 @@
 package definitions;
 
 import com.example.musicstreamingapi.MusicStreamingApiApplication;
+import com.example.musicstreamingapi.service.UserService;
 import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.logging.Logger;
@@ -12,5 +16,14 @@ public class UserControllertestDefs {
 
     private static final Logger logger = Logger.getLogger(PlaylistControllerTestDefs.class.getName());
 
+    private static Response response;
+
+    private static RequestSpecification request;
+
+    private String token;
+    private String message;
+
+    @Autowired
+    private UserService userService;
 
 }
